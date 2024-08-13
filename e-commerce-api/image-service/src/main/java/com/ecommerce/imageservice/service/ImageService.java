e1b05d5e327c0e6af21c80052a74562dcb053085
Ricky LT Wong <rickywonglt15@outlook.com>
2024-08-13 13:56:49 -0400

    feat: upload images to bucket on inital run

--- e-commerce-api/image-service/src/main/java/com/ecommerce/imageservice/service/ImageService.java
+++ e-commerce-api/image-service/src/main/java/com/ecommerce/imageservice/service/ImageService.java
@@ -1,8 +1,10 @@
 package com.ecommerce.imageservice.service;
 
 import io.minio.*;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,6 +19,65 @@ public class ImageService {
 
     @Value("${minio.bucket-name}")
     private String bucketName;
+
+    @PostConstruct
+    public void init() {
+        createBucketIfNotExists();
+        uploadInitialImages();
+    }
+
+    private void createBucketIfNotExists() {
+        try {
+            boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+            if (!found) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+                System.out.println("Bucket '" + bucketName + "' created.");
+            } else {
+                System.out.println("Bucket '" + bucketName + "' already exists.");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Error checking or creating bucket in MinIO", e);
+        }
+    }
+
+    public void uploadInitialImages() {
+        try {
+            String[] imagePaths = {
+                    "ipad.jpg",
+                    "ipadpro.jpg",
+                    "iphone12.jpg",
+                    "iphone13.jpg",
+                    "iphonese.jpg",
+                    "macbook.jpg"
+            };
+
+            for (String imagePath : imagePaths) {
+                uploadResourceImage(imagePath);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Error uploading initial images to MinIO", e);
+        }
+    }
+
+    private void uploadResourceImage(String resourcePath) {
+        try {
+            ClassPathResource resource = new ClassPathResource(resourcePath);
+            InputStream inputStream = resource.getInputStream();
+
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(resourcePath)
+                            .stream(inputStream, resource.contentLength(), -1)
+                            .contentType("image/jpeg")
+                            .build()
+            );
+
+            System.out.println("Uploaded image: " + resourcePath);
+        } catch (Exception e) {
+            throw new RuntimeException("Error uploading file " + resourcePath + " to MinIO", e);
+        }
+    }
 
 
     public String uploadImage(MultipartFile file) {
