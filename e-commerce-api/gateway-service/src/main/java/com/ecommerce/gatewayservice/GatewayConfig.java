27b81b61914ad6d9f8c1d86b651abffb7543cbba
Ricky LT Wong <rickywonglt15@outlook.com>
2024-08-14 09:42:02 -0400

    fix:route locator

--- e-commerce-api/gateway-service/src/main/java/com/ecommerce/gatewayservice/GatewayConfig.java
+++ e-commerce-api/gateway-service/src/main/java/com/ecommerce/gatewayservice/GatewayConfig.java
@@ -12,38 +12,20 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class GatewayConfig {
 
-//    @Bean
-//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route("products", r -> r.path("/api/products/**")
-//                        .filters(f -> f.rewritePath("/api/products/(?<segment>.*)", "/products/${segment}"))
-//                        .uri("lb://product-service"))
-//                .route("orders", r -> r.path("/api/orders/**")
-//                        .filters(f -> f.rewritePath("/api/orders/(?<segment>.*)", "/orders/${segment}"))
-//                        .uri("lb://order-service"))
-//                .route("admin", r -> r.path("/api/admin/**")
-//                        .filters(f -> f.rewritePath("/api/admin/(?<segment>.*)", "/admin/${segment}"))
-//                        .uri("lb://admin-service"))
-//                .route("images", r -> r.path("/api/images/**")
-//                        .filters(f -> f.rewritePath("/api/images/(?<segment>.*)", "/images/${segment}"))
-//                        .uri("lb://image-service"))
-//                .build();
-//    }
-
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("products", r -> r.path("/api/products/**")
-                        .filters(f -> f.rewritePath("/api/products/(?<remainingPath>.*)", "/products/${remainingPath}"))
+                        .filters(f -> f.rewritePath("/api/products/(?<remainingPath>.*)", "/api/products/${remainingPath}"))
                         .uri("lb://product-service"))
                 .route("orders", r -> r.path("/api/orders/**")
-                        .filters(f -> f.rewritePath("/api/orders/(?<remainingPath>.*)", "/orders/${remainingPath}"))
+                        .filters(f -> f.rewritePath("/api/orders/(?<remainingPath>.*)", "/api/orders/${remainingPath}"))
                         .uri("lb://order-service"))
                 .route("admin", r -> r.path("/api/admin/**")
-                        .filters(f -> f.rewritePath("/api/admin/(?<remainingPath>.*)", "/admin/${remainingPath}"))
+                        .filters(f -> f.rewritePath("/api/admin/(?<remainingPath>.*)", "/api/admin/${remainingPath}"))
                         .uri("lb://admin-service"))
                 .route("images", r -> r.path("/api/images/**")
-                        .filters(f -> f.rewritePath("/api/images/(?<remainingPath>.*)", "/images/${remainingPath}"))
+                        .filters(f -> f.rewritePath("/api/images/(?<remainingPath>.*)", "/api/images/${remainingPath}"))
                         .uri("lb://image-service"))
                 .build();
     }
